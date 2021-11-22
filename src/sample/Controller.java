276529c852e0afa722153114e0febf5e4425236a
Leha <maltsev0017@gmail.com>
2021-11-22 13:46:07 +0300

    Task to lesson 4 is done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+
 public class Controller {
+    StringBuilder textAreaValue = new StringBuilder();
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    private void sendText() {
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if(event.getCode() == KeyCode.ENTER) {
+                    btnSend();
+                }
+            }
+        });
+    }
+
+    public void btnSend() {
+        textAreaValue.append(textField.getText());
+        textArea.setText(textAreaValue.toString());
+        textField.setText("");
+    }
 }
